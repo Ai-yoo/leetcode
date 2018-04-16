b5a1d8118fd80278f914f5e083c7e9a3e456318b
Ai-yoo <duzhentong123@foxmail.com>
2018-04-16 15:32:01 +0800

    删除链表倒数第n个数

---
+++ linkedlist/Practice19.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+/**
+ * Created with IDEA
+ *
+ * @author duzhentong
+ * @Date 2018/4/16
+ * @Time 11:31
+ */
+public class Practice19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+
+        return head;
+    }
+}
