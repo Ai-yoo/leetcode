126e703588bf23836be22d793fba98f614298d4c
Ai-yoo <duzhentong123@foxmail.com>
2018-06-28 07:47:16 +0800

    sql

--- sql/practice.sql
+++ sql/practice.sql
@@ -33,7 +33,52 @@
 select p.FirstName,p.LastName,a.City,a.State from person p left outer join Address a on p.PersonId=a.PersonId
 select p.FirstName,p.LastName,a.City,a.State from person p left join Address a on p.PersonId=a.PersonId
 
+-- 176
+-- 编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary） 。
+--
+-- +----+--------+
+-- | Id | Salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+--
+-- 例如上述 Employee 表，SQL查询应该返回 200 作为第二高的薪水。如果不存在第二高的薪水，那么查询应返回 null。
+--
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
 
+select MAX(Salary) as SecondHighestSalary
+from Employee
+where Salary < (select MAX(Salary) from Employee);
+
+select IFNULL (
+        (select Salary
+        from Employee
+        group by Salary
+        order by Salary DESC
+        limit 1,1),null
+    ) as SecondHighestSalary
+
+
+--177
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    declare n1 int;
+    set n1=n-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      select IFNULL((select Salary from Employee group by Salary order by Salary desc limit n1,1),null) as getNthHighestSalary
+  );
+END
+
+--182
+select Email from Person group by Email having count(*)>1
 
 -- 627
 
